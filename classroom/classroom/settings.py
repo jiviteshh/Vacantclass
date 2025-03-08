4a185b2ee6f639dfb86723e911735a58948cd58a
jiviteshh <jivinaragam@gmail.com>
2025-03-08 11:49:41 +0530

    Added Render URL to ALLOWED_HOSTS

--- classroom/classroom/settings.py
+++ classroom/classroom/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')  # Use env 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []  
+ALLOWED_HOSTS = ['vacantclassfinder.onrender.com']  
 
 # Application definition
 
